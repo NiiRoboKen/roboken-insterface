c5b37e9bf6c358f12615fb802e6b9b463e14335a
Sootta <147506050+Sootta@users.noreply.github.com>
2024-12-07 13:25:59 +0900

    円周率の定数変更

--- src/servo.h
+++ src/servo.h
@@ -1,14 +1,13 @@
 #ifndef ROBOKEN_INTERFACE_MOTOR
 #define ROBOKEN_INTERFACE_MOTOR
+#include <cmath>
 
 class Servo {
     public:
     virtual void set_degree(int angle) = 0;
     virtual void set_radian(double angle) {
-        set_degree(angle * 180 / PI);
+        set_degree(angle * 180 / M_PI);
     }
-    private:
-    const double PI = 3.14159265358979;
 };
 
 #endif
